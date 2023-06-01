f0294270bec39df4dd7c42c54525acb02ba89086
Julia Kotovich <37921248+Julia-Kotovich@users.noreply.github.com>
2023-06-01 11:50:12 +0200

    Create 013_AVL_Tree_Python_v2.py
    This implementation includes the insert and search operations, as well as the rotations (_rotate_left and _rotate_right) to maintain the balance of the AVL tree. The height of each node is tracked and used to calculate the balance factor. The _insert method recursively inserts the key while ensuring the balance of the tree by performing appropriate rotations.

---
+++ Data_structures/013_AVL_Tree_Python_v2.py
@@ -0,0 +1,101 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if node is None:
+            return Node(key)
+        
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        else:
+            node.right = self._insert(node.right, key)
+        
+        node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
+        
+        balance = self._get_balance(node)
+        
+        if balance > 1:
+            if key < node.left.key:
+                return self._rotate_right(node)
+            else:
+                node.left = self._rotate_left(node.left)
+                return self._rotate_right(node)
+        
+        if balance < -1:
+            if key > node.right.key:
+                return self._rotate_left(node)
+            else:
+                node.right = self._rotate_right(node.right)
+                return self._rotate_left(node)
+        
+        return node
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node is not None
+        
+        if key < node.key:
+            return self._search(node.left, key)
+        else:
+            return self._search(node.right, key)
+
+    def _get_height(self, node):
+        if node is None:
+            return 0
+        return node.height
+
+    def _get_balance(self, node):
+        if node is None:
+            return 0
+        return self._get_height(node.left) - self._get_height(node.right)
+
+    def _rotate_left(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+        return y
+
+    def _rotate_right(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+        return y
+
+
+# Usage:
+avl_tree = AVLTree()
+avl_tree.insert(10)
+avl_tree.insert(20)
+avl_tree.insert(5)
+avl_tree.insert(15)
+avl_tree.insert(30)
+
+print(avl_tree.search(10))  # Output: True
+print(avl_tree.search(25))  # Output: False
