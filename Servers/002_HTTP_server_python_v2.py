4f801bf6ac66db90fd7f57cd94214ddffcb8add2
Julia Kotovich <37921248+Julia-Kotovich@users.noreply.github.com>
2023-06-02 14:30:48 +0200

    Create 002_HTTP_server_python_v2.py
    In this implementation, we use the socket module to create a TCP socket, bind it to the specified host and port, and listen for incoming connections. Once a client connects, we accept the connection and receive the client's request using the recv() method.
    
    We then send the HTTP response back to the client using the sendall() method of the client socket. The response is a simple HTML document stored in the response variable.
    
    Finally, we close the client connection and continue listening for new connections.
    
    Please note that this is a basic example and does not handle more complex features such as parsing HTTP headers, handling different HTTP methods, or supporting concurrent connections. Building a complete and robust HTTP server from scratch can be a complex task, and it is usually recommended to use existing libraries or frameworks for real-world scenarios.

---
+++ Servers/002_HTTP_server_python_v2.py
@@ -0,0 +1,42 @@
+import socket
+
+# Define server settings
+HOST = 'localhost'  # Replace with your server hostname or IP address
+PORT = 8000  # Replace with the desired server port number
+
+# Define HTTP response
+response = """HTTP/1.1 200 OK
+Content-Type: text/html
+
+<html>
+<body>
+<h1>Hello, World!</h1>
+</body>
+</html>
+"""
+
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the host and port
+server_socket.bind((HOST, PORT))
+
+# Listen for incoming connections
+server_socket.listen(1)
+
+print(f"HTTP server listening on {HOST}:{PORT}")
+
+while True:
+    # Accept a client connection
+    client_socket, client_address = server_socket.accept()
+    print(f"New client connected: {client_address[0]}:{client_address[1]}")
+
+    # Receive the client request
+    request = client_socket.recv(1024).decode()
+    print(f"Received request:\n{request}")
+
+    # Send the HTTP response
+    client_socket.sendall(response.encode())
+
+    # Close the client connection
+    client_socket.close()
